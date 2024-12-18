ffdb71acadbb848652af33422ee475a0ccf78a1b
sergius69 <sergiurbanejasirigu@gmail.com>
2024-12-18 16:30:04 +0100

    incluyo forma ingeniosa de despedirse
    ciao pescao!

--- Trabajogithub/src/ejerciciogit.java
+++ Trabajogithub/src/ejerciciogit.java
@@ -4,6 +4,7 @@ public class ejerciciogit {
 	public static void main(String[] args) {
 		System.out.println("Holi wankenobi");
 		System.out.println("Agur yogur!");	
+		System.out.println("Ciao pescao!");	
 		}
 
 }
